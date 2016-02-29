7f646b7325c9bdc266668ff18210462a23ed082d
Daniel Dunn <orbitaldan@gmail.com>
2016-02-29 00:46:57 -0600

    License, fixed script comments
    Just a few cleanups

--- CustomPrompt.ps1
+++ CustomPrompt.ps1
@@ -1,4 +1,17 @@
-﻿#--------------------------------------------------------------------
+﻿# PSCustomPrompt.ps1
+# Makes the PowerShell prompt look just a little bit nicer. :)
+#
+# Copyright (c) 2016 Daniel J. Dunn II
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+#    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+# TL;DR: Gimme a namedrop if it's useful, don't blame me if it wrecks your computer. Otherwise, have fun!
+
+#--------------------------------------------------------------------
 # Terminal Enabling Code From:
 # http://www.nivot.org/blog/post/2016/02/04/Windows-10-TH2-%28v1511%29-Console-Host-Enhancements
 Add-Type -MemberDefinition @"
@@ -73,15 +86,23 @@ $global:Symbols.Powerline = @{
 
 # Helper function to build escape sequences. Codes 30-37 select
 # foreground color (0-7), codes 40-47 select background color
-# (0-7).  The codes correspond to the first eight colors in the
-# console host properties dialog, and the first eight colors in
-# the System.ConsoleColor enumeration.
+# (0-7). I wish I could tell you an easy way to figure out which
+# is which, but all I could do was just try them and see.
 function SetGraphics([int[]]$m)     { 
   "$([char]0x1b)[$([String]::Join(';',($m|%{$_.ToString()})))m"; 
 }
 
 # Create a template for the prompt so that we don't waste time
-# reconstructing it every command
+# reconstructing it every command.  Note that I use DarkCyan
+# (Index 6) as the background color for the prompt, but I
+# edited it in the console to RGB(0,90,135) so that it looks
+# nicer.  I also used that color on a black background for
+# the initial symbol to make it stand apart.  Near the end,
+# I switch to DarkCyan foreground and DarkMagenta (Index 5)
+# background. (Default PowerShell recolors DarkMagenta to the
+# dark-blue background color, and DarkYellow to the default
+# grayish text color.) The 'RightBlock' character from Powerline
+# creates the nice transition between the two.
 $global:PromptTemplate =              `
     (SetGraphics 36, 40) +            `
     "{0}" +                           `
